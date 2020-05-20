bdf470f06777b4eccc65f1bb1a4e360d57f7029d
Pantea Tavakolian <59155694+ptavakolian@users.noreply.github.com>
2020-05-20 13:29:12 -0400

    Add files via upload
    First, randomforest.py code needs to be implemented. The input directory for this file is Training_WFDB. It also calls get_12ECG_features. 
    The command to run this code on terminal is: python3 randomforest.py ./Training_WFDB
    Second, driver.py needs to be run. The input directory for this file is also Training_WFDB and the output directory needs to be specified (it generates a directory based on the specified name). This program calls get_12ECG_features and run_12ECG_classifier.py. 
    The command to run this code on terminal is: python3 driver.py ./Training_WFDB ./output

---
+++ run_12ECG_classifier.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import numpy as np
+import joblib
+from get_12ECG_features import get_12ECG_features
+from sklearn import model_selection
+
+def run_12ECG_classifier(data,header_data,classes,model):
+
+    num_classes = len(classes)
+    current_label = np.zeros(num_classes, dtype=int)
+    current_score = np.zeros(num_classes)
+    # Use your classifier here to obtain a label and score for each class. 
+    features=np.asarray(get_12ECG_features(data,header_data))
+    feats_reshape = features.reshape(1,-1)
+    label = model.predict(feats_reshape)
+    score = model.predict_proba(feats_reshape)
+    score=np.squeeze(score)
+
+    current_label[label] = 1
+
+    for i in range(num_classes):
+        current_score[i] = np.array(score[i][1])
+
+    return np.squeeze(label), current_score
+
+
+
+def load_12ECG_model():
+    # load the model from disk 
+    filename='finalized_model.sav'
+    loaded_model = joblib.load(filename)
+
+    return loaded_model
