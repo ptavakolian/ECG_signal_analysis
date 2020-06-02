4e18e810a1b5d805c68d3667ffc32bc72b1e46e4
Ray Duran <rayd@extron.com>
2020-06-01 19:46:02 -0700

    This code breaks the normal flow of operation. It is intermediate code, with portions of the driver commented out so that I can break using Pycharm
    Changes
    1. driver.py
     a. Added function get_prob_distr() to put prob distr of class in a data frame
     b. Commented out stuff for classifier 1
     c. Added stuff to form a model for classifer 2
    2. Made the function load_12ECG_model accept a filename
    3. Added a new file: randomforest.py ( You need to run this file to generate new model       "finalized_model2.sav"
    
    This code breaks at the line 17 label = model.predict(feats_reshape)

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import sys
+import pandas as pd
 from scipy.io import loadmat
 from run_12ECG_classifier import load_12ECG_model, run_12ECG_classifier
 
@@ -38,6 +39,29 @@ def save_challenge_predictions(output_directory, filename, scores, labels, class
         f.write(recording_string + '\n' + class_string + '\n' + label_string + '\n' + score_string + '\n')
 
 
+#rbd_Debug_begin
+def get_prob_distr(output_directory, files):
+    distr= set()
+    entry = []
+    df1 = pd.DataFrame()
+    for f in files:
+        input_file = os.path.join(output_directory, f)
+        patient = f
+        with open(input_file, 'r') as f:
+             tmp = f.readlines()[3:]
+             for c in tmp:
+                 distr.add(c.strip())
+                 tmp2 = c.split(',')
+                 for d in tmp2:
+                     entry.append(d.rstrip("\n"))
+                 df2 = pd.DataFrame({patient : entry}, index=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+                 df1.reset_index(drop=True, inplace=True)
+                 df2.reset_index(drop=True, inplace=True)
+                 df1 = pd.concat([df1, df2], axis=1, sort=False)
+                 entry.clear()
+
+    return (df1)
+
 # Find unique number of classes
 def get_classes(input_directory, files):
 
@@ -57,11 +81,22 @@ def get_classes(input_directory, files):
 
 if __name__ == '__main__':
     # Parse arguments.
-    if len(sys.argv) != 3:
-        raise Exception('Include the input and output directories as arguments, e.g., python driver.py input output.')
 
-    input_directory = sys.argv[1]
-    output_directory = sys.argv[2]
+    #rbd_Debug
+    #if len(sys.argv) != 3:
+    #    raise Exception('Include the input and output directories as arguments, e.g., python driver.py input output.')
+    #rbd_Debug
+
+    #rbd_Debug
+    #input_directory = sys.argv[1]
+    #output_directory = sys.argv[2]
+    #rbd_Debug
+    input_directory = "../Training_WFDB"
+    output_directory = "../output_data_exp_test4"
+    output_directory2 = "../output_data_rf2"
+    model2 = "finalized_model2.sav"
+
+
 
     # Find files.
     input_files = []
@@ -73,6 +108,10 @@ if __name__ == '__main__':
         os.mkdir(output_directory)
 
     classes = get_classes(input_directory, input_files)
+
+    """"
+    #rbd_Debug_begin : We are debugging and want to skip the creation of class outputs
+    So that we can test our snippet of code that reads in CSV files
 
     # Load model.
     print('Loading 12ECG model...')
@@ -90,5 +129,40 @@ if __name__ == '__main__':
         # Save results.
         
         save_challenge_predictions(output_directory, f, current_score, current_label, classes)
-
+        
+    #rbd_Debug_end
+    """
     print('Done.')
+    #rbd_debug
+    # Parse Distributions from one classifier
+    # Find files.
+    parse_files = []
+    for f in os.listdir(output_directory):
+        if os.path.isfile(os.path.join(output_directory, f)) and not f.lower().startswith('.') and f.lower().endswith('csv'):
+            parse_files.append(f)
+
+    print('Debug: Start reading CSV files of classifier')
+    df1 = get_prob_distr(output_directory, parse_files)
+    print('Debug: Done Reading CSV files of classifier')
+    df1
+    print('Done with forming DataFrame for Random Forest')
+
+    # Load model.
+    print('Loading 12ECG model for RF 2...')
+    model = load_12ECG_model(model2)
+
+    # Iterate over files.
+    print('Extracting 12ECG features...')
+    num_files = len(input_files)
+
+    for i, f in enumerate(input_files):
+        print('    {}/{}...'.format(i + 1, num_files))
+        tmp_input_file = os.path.join(input_directory, f)
+        data, header_data = load_challenge_data(tmp_input_file)
+        current_label, current_score = run_12ECG_classifier(data, header_data, classes, model2)
+        # Save results.
+
+        save_challenge_predictions(output_directory2, f, current_score, current_label, classes)
+
+    print('Done Random Forest 2.')
+    dummy_debug = 0
